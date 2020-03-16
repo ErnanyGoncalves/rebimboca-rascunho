738e5160193fa343faa0feea7b91d54d95d4ba43
Ernany Gonçalves <goncalves.ernany@gmail.com>
2020-03-16 16:49:05 -0300

    Tutorial Parte 5 feita

--- src/Scene1.js
+++ src/Scene1.js
@@ -1,7 +1,9 @@
 import background from "./assets/background.png";
-import ship1 from "./assets/ship.png";
-import ship2 from "./assets/ship2.png";
-import ship3 from "./assets/ship3.png";
+import ship1SS from "./assets/spritesheets/ship.png";
+import ship2SS from "./assets/spritesheets/ship2.png";
+import ship3SS from "./assets/spritesheets/ship3.png";
+import boomSS from "./assets/spritesheets/explosion.png";
+
 
 export default class Scene1 extends Phaser.Scene{
     constructor(){
@@ -10,9 +12,26 @@ export default class Scene1 extends Phaser.Scene{
 
     preload(){
         this.load.image("background",background);
-        this.load.image("ship",ship1);
-        this.load.image("ship2",ship2);
-        this.load.image("ship3",ship3);
+        this.load.spritesheet("ship",ship1SS,{
+            frameWidth:16,
+            frameHeight:16
+        });
+
+        this.load.spritesheet("ship2",ship2SS,{
+            frameWidth:32,
+            frameHeight:16
+        });
+
+        this.load.spritesheet("ship3",ship3SS,{
+            frameWidth:32,
+            frameHeight:32
+        });
+        
+        
+        this.load.spritesheet("explosion",boomSS,{
+            frameWidth:16,
+            frameHeight:16
+        });
     }
 
     create(){
